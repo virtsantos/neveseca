a3105e03669123a55d38184f504a3167226000c4
D1asAndre <126107754+D1asAndre@users.noreply.github.com>
2023-03-27 19:44:06 +0100

    Update neveseca.java

--- neveseca.java
+++ neveseca.java
@@ -1,5 +1,5 @@
 public class neveseca {
     public static void main(String[] args) {
-        System.out.println("A neve é molhada !!!");
+        System.out.println("A neve é muhada !!!");
     }
 }
